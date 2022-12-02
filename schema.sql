a8d0fd328a258cc37bb9dece28124ad6c55d054f
morax0 <moraxx@163.com>
2022-12-02 13:18:12 -0500

    database
    part 2

---
+++ schema.sql
@@ -0,0 +1,24 @@
+
+CREATE TABLE IF NOT EXISTS hourly_weather
+    (id INTEGER PRIMARY KEY,
+    DATE DATE,
+    LATITUDE INTEGER,
+    LONGITUDE INTEGER,
+    NAME TEXT,
+    BackupName TEXT,
+    hours INTEGER,
+    newDATE DATE)
+
+CREATE TABLE IF NOT EXISTS daily_weather
+    (id INTEGER PRIMARY KEY,
+    DATE DATE,
+    LATITUDE INTEGER,
+    LONGITUDE INTEGER,
+    NAME TEXT,
+    BackupName TEXT,
+    days INTEGER,
+    newDATE DATE)
+
+TODO
+
+TODO
